f59c11d3275074e8c5420ee3d51cd7a636b53363
perwriters <perwriters@gmail.com>
2023-12-01 20:17:59 +0300

    Complete the routing section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import React from "react";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ExerciseDetails from "./pages/ExerciseDetails";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise" element={<ExerciseDetails />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 };
 
 export default App;
