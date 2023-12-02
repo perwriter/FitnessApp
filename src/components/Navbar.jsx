db9a043e8e0d753d5746c73101c81a5d1cd52d16
perwriters <perwriters@gmail.com>
2023-12-03 00:29:28 +0300

    Finished with the Nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,38 @@
 import React from "react";
+import { Stack } from "@mui/material";
 import { Link } from "react-router-dom";
+import Logo from "../assets/images/logo.png";
 
 const Navbar = () => {
   return (
     <>
-      <Link>Navbar</Link>
+      <Stack>
+        <Link to="/">
+          <img
+            src={Logo}
+            alt="Logo"
+            style={{ width: "48px", height: "48px", margin: "" }}
+          />
+        </Link>
+      </Stack>
+      <Stack direction="row" spacing={2} alignItems="center">
+        <Link
+          to="/"
+          style={{
+            textDecoration: "none",
+            color: "#3a1212",
+            borderBottom: "2px solid #ff2625",
+          }}
+        >
+          Home
+        </Link>
+        <a
+          href="#exercises"
+          style={{ textDecoration: "none", color: "#3a1212" }}
+        >
+          Exercises
+        </a>
+      </Stack>
     </>
   );
 };
