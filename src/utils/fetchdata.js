36632101d90317518be2ec78362fcd13f350fe20
peter <perwriters@gmail.com>
2023-12-04 11:00:14 +0300

    Update fetchdata.js

--- src/utils/fetchdata.js
+++ src/utils/fetchdata.js
@@ -7,7 +7,7 @@ export const fetchData = async (url, options) => {
 export const exerciseOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": prcess.evn.REACT_APP_RAPID_API_KEY,
+    "X-RapidAPI-Key": prcess.env.REACT_APP_RAPID_API_KEY,
     "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
   },
 };
